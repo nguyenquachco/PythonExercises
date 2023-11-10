6eaa45c98f8d21833269aab9cc77d7933b37dc6f
nguyenquachco <nguyenquachco@gmail.com>
2023-11-10 13:18:11 +0700

    Một số bài tập khác với lệnh print

---
+++ 1.Print/print_raw.py
@@ -0,0 +1,7 @@
+# Python program to use raw strings
+ 
+# Define string with r before quotes
+# and assign to variable
+s1 = r'Python\nis\easy\to\learn'
+ 
+print(s1)
--- 1.Print/print_roll2.py
+++ 1.Print/print_roll2.py
@@ -1,14 +1,19 @@
 # In ra dòng chữ "Cộng hòa xã hội chủ nghĩa Việt Nam" xoay vòng
 # Dùng vòng lặp while kết hợp với biến xâu 
 # Dùng hàm time.sleep() để dừng một chút mỗi lần xoay vòng
-# Dùng hàm keyboard.is_pressed() để thoát khi có phím bấm (không cần phải bấm Ctrl + C)
+# Dùng hàm keyboard.is_pressed() để thoát khi phím space được bấm (không cần phải bấm Ctrl + C)
 import time
 import keyboard
 str = "Cộng hòa xã hội chủ nghĩa Việt Nam  "
+tim_cnt = 0
 while True:
-    print(f"\r{str}", end = '\0')
-    str = str[1:] + str[0]
-    time.sleep(1)
-    if (keyboard.is_pressed('a')):
+    if tim_cnt == 0:
+        print(f"\r{str}", end = '\0')
+        str = str[1:] + str[0]
+    time.sleep(0.01)
+    tim_cnt = tim_cnt + 1
+    if tim_cnt >= 100:
+        tim_cnt = 0
+    if keyboard.is_pressed('space'):
         break
 
